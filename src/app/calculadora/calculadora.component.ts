a70cbbec5bcd68775f873cfa5355759a026e184b
yesipizarro <93060508+yesipizarro@users.noreply.github.com>
2022-10-03 17:10:45 -0500

    se crea el formulario (#1)
    * se crea el formulario
    
    * se crea archivo de variables de colores
    
    * se elimina metodos
    
    Co-authored-by: Yesica <yesi@MacBook-Air-de-Yesica.local>

---
+++ src/app/calculadora/calculadora.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.scss']
+})
+export class CalculadoraComponent {
+
+
+
+}
