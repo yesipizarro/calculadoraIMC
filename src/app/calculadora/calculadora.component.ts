05476bbf39adc62ef5783479370c19d43861906f
yesipizarro <93060508+yesipizarro@users.noreply.github.com>
2022-10-04 17:02:48 -0500

    se genera el calculo y se muestra el resultado (#2)
    * se genera el calculo y se muestra el resultado
    
    * se arreglan comentarios
    
    Co-authored-by: Yesica <yesi@MacBook-Air-de-Yesica.local>

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ServicioIMCService, DatosIMC, IMC } from '../servicio-imc.service';
 
 @Component({
   selector: 'app-calculadora',
@@ -7,6 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraComponent {
 
+  imcs = IMC;
+
+  constructor(private formularioServiceIMC: ServicioIMCService) { }
+
+  formularioIMC: FormGroup = new FormGroup({
+    estatura: new FormControl(undefined, [Validators.required, Validators.min(1)]),
+    peso: new FormControl(undefined, [Validators.required, Validators.min(1)])
+  })
+
+  calcular() {
+    let valorestatura = this.formularioIMC.value.estatura;
+    let valorpeso = this.formularioIMC.value.peso;
+    this.formularioServiceIMC.calcularIMC(valorestatura, valorpeso);
+
+  }
+
+  get mostrarResultado() {
+    let paraMostrar = this.formularioServiceIMC.resultado;
+    if (!paraMostrar) return IMC.NEUTRO;
+    if (paraMostrar < 18) {
+      return IMC.INFERIOR;
+    } else if (paraMostrar >= 18.5 && paraMostrar <= 24.9) {
+      return IMC.NORMAL;
+    } else if (paraMostrar >= 25.0 && paraMostrar <= 29.9) {
+      return IMC.SUPERIOR;
+    } else {
+      return IMC.OBESIDAD;
+    }
+  }
 
 
 }
